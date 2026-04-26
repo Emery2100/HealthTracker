d4f5f02828d41023e5c0934f4188ecacc179eefe
David Emery <Emery2100@yahoo.com>
2026-04-26 11:20:47 -0700

    Initial Commit

---
+++ HealthTracker Watch App/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker Watch App
+//
+//  Created by David Emery on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
